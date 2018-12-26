a03e7fc2885dad0f499af45381778c13df6fc26d
PierrotAWB <andrewwang298@gmail.com>
2018-12-26 11:45:00 -0500

    Delete dummy data; Create Post DB; Register Post DB in admin.py

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author': 'AndrewWB',
-		'title' : 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'December 25, 2018'
-	},
-	{
-		'author': 'CoreyMS',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'December 26, 2018'
-	}
-]
+from .models import Post
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
