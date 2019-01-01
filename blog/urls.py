3427e3b3000ab6e55b0e64fe283b0c6d262b687c
PierrotAWB <andrewwang298@gmail.com>
2019-01-01 11:04:23 -0500

    Add list, detail, update, create, and delete class-based views so that users can interact with posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
+from .views import (
+    PostListView, 
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView, 
+    PostDeleteView)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('', PostListView.as_view(), name='blog-home'),
+    # pk is a primary key
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    # a create view looks at a different location; 
+    # rather than check blog/post_create.html as you might
+    # guess, it looks for blog/post_form.html. Note that this
+    # is shared by the Update view.
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('about/', views.about, name='blog-about')
 ]
