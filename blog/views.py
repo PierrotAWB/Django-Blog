2beef2d311fca6f624dd1e8ed6339deb21723593
PierrotAWB <andrewwang298@gmail.com>
2019-01-03 21:58:54 -0500

    Add blog posts and introduce pagination

--- blog/views.py
+++ blog/views.py
@@ -21,6 +21,7 @@ class PostListView(ListView):
 	template_name = 'blog/home.html' 	# <app>/<model>_<viewtype>.html
 	context_object_name = 'posts'       # the equivalent of "context"
 	ordering = ['-date_posted']
+	paginate_by = 5
 
 class PostDetailView(DetailView):
 	model = Post
