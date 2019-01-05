21787f0b4fb8f5ad4596fb33a6d747042bcfa46c
PierrotAWB <andrewwang298@gmail.com>
2019-01-05 17:42:50 -0500

    Add 'form.save()' line to register in /users/views.py

--- users/views.py
+++ users/views.py
@@ -5,17 +5,17 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
-	if request.method == 'POST':
-		form = UserRegisterForm(request.POST)
-		if form.is_valid():
-			email = form.cleaned_data.get('email')
-			username = form.cleaned_data.get('username')
-			messages.success(request, f'Your account has been created! You are now able to log in.')
-			return redirect('login')
-	else: 
-		form = UserRegisterForm()
-	return render(request, 'users/register.html', {'form': form})
-
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+    
 @login_required
 def profile(request):
 	if request.method == 'POST':
